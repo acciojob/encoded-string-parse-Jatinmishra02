eb299a416fb879dbb1835aeb646d8ced9df2e716
Jatin Mishra <104919571+Jatinmishra02@users.noreply.github.com>
2024-06-25 00:04:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const parseCode = (str) => {
-  // your code here
+  // Split the string by '0'
+  let parts = str.split('0');
+  
+  // Filter out any empty strings that result from consecutive zeros
+  parts = parts.filter(part => part !== '');
+
+  // Destructure the parts into firstName, lastName, and id
+  const [firstName, lastName, id] = parts;
+
+  // Return the resulting object
+  return { firstName, lastName, id };
 };
 
 // Do not change the code below
